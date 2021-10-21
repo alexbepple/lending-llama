f96f07672ccef0000f0ff1c5be857b3bb3240657
Alex Bepple <alex@bepple.de>
2021-10-21 18:54:00 +0200

    . proxy backend under /api

--- frontend/snowpack.config.mjs
+++ frontend/snowpack.config.mjs
@@ -1,3 +1,5 @@
+import proxy from 'http2-proxy';
+
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
@@ -12,7 +14,18 @@ export default {
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
-  ],
+    {
+      src: '/api/.*',
+      dest: (req, res) => {
+        req.url = req.url.replace(/^\/api/, '/'); // remove /api prefix
+
+        return proxy.web(req, res, {
+          hostname: 'localhost',
+          port: 8080,
+        });
+      },
+    },
+],
   optimize: {
     /* Example: Bundle your final build: */
     // "bundle": true,
