f37e6f85955e038774d1499d0342f79941d7b7e9
Alex Bepple <alex@bepple.de>
2021-10-21 19:01:04 +0200

    $$$ walking skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,16 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import s from './App.module.css'
 
-export const App = () => (
-  <div className={s.emphasis}>Hello, React!</div>
-)
+export const App = () => {
+  const [greeting, setGreeting] = useState('')
+  useEffect(() => {
+    fetch('/api/greeting')
+      .then(x=>x.text())
+      .then(JSON.parse)
+      .then(x => x.content)
+      .then(setGreeting)
+  }, [])
+  return (
+    <div className={s.emphasis}>{greeting}</div>
+  );
+}
