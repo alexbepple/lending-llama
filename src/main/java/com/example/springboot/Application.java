7fbb8c989c66043052b90e1ba0c050666d0eaf31
Alex Bepple <alex@bepple.de>
2021-10-21 15:58:39 +0200

    . enable request logging

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -13,6 +13,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.setProperty("spring.devtools.livereload.enabled", "false");
+		System.setProperty("logging.level.web", "DEBUG");
 		SpringApplication.run(Application.class, args);
 	}
 
