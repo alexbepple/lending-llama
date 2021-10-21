756773cbdd4ec74757e4fa68e43c1fa46e721e2d
Alex Bepple <alex@bepple.de>
2021-10-21 18:29:08 +0200

    . enable HMR

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,3 +4,9 @@ import {App} from "./App";
 
 const app = <React.StrictMode><App/></React.StrictMode>;
 ReactDOM.render(app, document.getElementById('root'));
+
+// Hot Module Replacement (HMR)
+// Learn more: https://www.snowpack.dev/concepts/hot-module-replacement
+if (undefined /* [snowpack] import.meta.hot */ ) {
+  undefined /* [snowpack] import.meta.hot */ .accept();
+}
