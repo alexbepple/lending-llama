b4327d7f0144c53cefa502dce58dd20f9896ee0e
Alex Bepple <alex@bepple.de>
2021-10-21 15:55:19 +0200

    . disable LiveReload server

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Bean;
 public class Application {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.devtools.livereload.enabled", "false");
 		SpringApplication.run(Application.class, args);
 	}
 
