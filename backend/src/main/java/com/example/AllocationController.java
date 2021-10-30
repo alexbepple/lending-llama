84e621d4a6bfa16b0df5b2bcad1242c015aefd60
Alex Bepple <alex@bepple.de>
2021-10-30 21:05:06 +0200

    $ /allocation: determine best tier

--- backend/src/main/java/com/example/AllocationController.java
+++ backend/src/main/java/com/example/AllocationController.java
@@ -4,6 +4,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.stream;
+
 @RestController
 public class AllocationController {
 
@@ -16,8 +22,16 @@ public class AllocationController {
     @GetMapping("/allocation")
     public Allocation getAllocation() {
         String url = "https://priceless-khorana-4dd263.netlify.app/btc-rates.json";
-        Platform platform = restTemplate.getForObject(url, Platform[].class)[0];
-        Platform.Tier tier = platform.getTiers()[0];
-        return new Allocation().setName(platform.getName()).setRate(tier.getRate());
+        Platform[] platforms = restTemplate.getForObject(url, Platform[].class);
+        List<PlatformTier> platformTiers = stream(platforms).flatMap(p -> stream(p.getTiers()).map(t ->
+            new PlatformTier()
+                .setName(p.getName())
+                .setRate(t.getRate())
+                .setMax(t.getMax())
+        ))
+            .sorted(Comparator.comparingDouble(PlatformTier::getRate).reversed())
+            .collect(Collectors.toList());
+        PlatformTier tier1 = platformTiers.get(0);
+        return new Allocation().setName(tier1.getName()).setRate(tier1.getRate());
     }
 }
