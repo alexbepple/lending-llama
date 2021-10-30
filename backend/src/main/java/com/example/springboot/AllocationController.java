e1eea7f2a7e79294839c1d02adf137b9deaba8c2
Alex Bepple <alex@bepple.de>
2021-10-30 20:24:56 +0200

    $ /allocation: fetch rates from external API
    push down fake

--- backend/src/main/java/com/example/springboot/AllocationController.java
+++ backend/src/main/java/com/example/springboot/AllocationController.java
@@ -2,12 +2,22 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class AllocationController {
 
+    private RestTemplate restTemplate;
+
+    public AllocationController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     @GetMapping("/allocation")
     public Allocation getAllocation() {
-        return new Allocation().setName("Ledn").setRate(6.25);
+        String url = "https://priceless-khorana-4dd263.netlify.app/btc-rates.json";
+        Platform platform = restTemplate.getForObject(url, Platform[].class)[0];
+        Platform.Tier tier = platform.getTiers()[0];
+        return new Allocation().setName(platform.getName()).setRate(tier.getRate());
     }
 }
