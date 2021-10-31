964e7faa82c31f61dc90f91f893b26a69a180b69
Alex Bepple <alex@bepple.de>
2021-10-31 14:03:28 +0100

    $ UI shell in line with corporate identity

--- frontend/src/AppShell.jsx
+++ frontend/src/AppShell.jsx
@@ -9,16 +9,7 @@ export const AppShell = ({children}) => (
         <div className="flex justify-between h-16">
           <div className="flex">
             <div className="flex-shrink-0 flex items-center">
-              <img
-                className="block lg:hidden h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                alt="Workflow"
-              />
-              <img
-                className="hidden lg:block h-8 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-800-text.svg"
-                alt="Workflow"
-              />
+              <span className="h-8 w-auto text-3xl">🦙 Lending Llama</span>
             </div>
           </div>
         </div>
