61b32cfd411d0bbaf2ed7da1f977282955ec0df9
Alex Bepple <alex@bepple.de>
2021-10-31 14:07:27 +0100

    . remove no longer needed dummy code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,9 @@
 import React, {useEffect, useState} from "react";
-import s from './App.module.css'
 import {useDispatch, useSelector} from "react-redux";
 import {Card, CircularMinusButton, CircularPlusButton} from "./presentation";
 import {CounterCard} from "./CounterCard";
 
 export const App = () => {
-  const [greeting, setGreeting] = useState('')
-  useEffect(() => {
-    fetch('/api/')
-      .then(x=>x.text())
-      .then(setGreeting)
-  }, [])
-
   const [allocation, setAllocation] = useState({})
   useEffect(() => {
     fetch('/api/allocation')
@@ -24,9 +16,6 @@ export const App = () => {
 
   return (
     <>
-      <Card>
-        <span className={s.emphasis}>{greeting}</span>
-      </Card>
       <Card>
         Best rate: {allocation.rate}% ({allocation.name})
       </Card>
