fbf74f4088f9d493aa1f3b498d37dcf83c272655
Alex Bepple <alex@bepple.de>
2021-11-01 22:53:18 +0100

    . clean up Snowpack config

--- frontend/snowpack.config.mjs
+++ frontend/snowpack.config.mjs
@@ -3,44 +3,29 @@ import proxy from 'http2-proxy';
 /** @type {import("snowpack").SnowpackUserConfig } */
 export default {
   mount: {
-    /* ... */
-         // directory name: 'build directory'
-         public: '/',
-       src: '/dist'
+    public: '/',
+    src: '/dist'
   },
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-postcss',
   ],
   routes: [
-    /* Enable an SPA Fallback in development: */
-    // {"match": "routes", "src": ".*", "dest": "/index.html"},
     {
       src: '/api/.*',
       dest: (req, res) => {
         req.url = req.url.replace(/^\/api/, ''); // remove /api prefix
-
         return proxy.web(req, res, {
           hostname: 'localhost',
           port: 8080,
         });
       },
     },
-],
-  optimize: {
-    /* Example: Bundle your final build: */
-    // "bundle": true,
-  },
-  packageOptions: {
-    /* ... */
-  },
+  ],
   devOptions: {
     port: 8081,
     open: 'none',
 
     tailwindConfig: './tailwind.config.js',
-  },
-  buildOptions: {
-    /* ... */
   },
 };
